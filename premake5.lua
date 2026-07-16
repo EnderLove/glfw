46372a78714e61abe895db8c5ac885c260f00470
EnderLove <cocpowergameplays@gmail.com>
2026-07-15 22:12:50 -0500

    PREMAKE5.LUA

--- premake5.lua
+++ premake5.lua
@@ -6,13 +6,13 @@ project "GLFW"
 	objdir    ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files {
-		"include/GLFW/glfw3.h"
-		"include/GLFW/glfw3native.h"
-		"src/glfw_config.h"
-		"src/init.c"
-		"src/input.c"
-		"src/monitor.c"
-		"src/vulkan.c"
+		"include/GLFW/glfw3.h",
+		"include/GLFW/glfw3native.h",
+		"src/glfw_config.h",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+		"src/vulkan.c",
 		"src/window.c"
 	}	
 
@@ -22,14 +22,14 @@ project "GLFW"
 		staticruntime "On"
 
 		files {
-			"src/win32_init.c"
-			"src/win32_joystic.c"
-			"src/win32_monitor.c"
-			"src/win32_time.c"
-			"src/win32_thread.c"
-			"src/win32_window.c"
-			"src/wgl_context.c"
-			"src/egl_context.c"
+			"src/win32_init.c",
+			"src/win32_joystic.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
 			"src/osmesa_context.c"
 		}
 
